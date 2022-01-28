cbebe9b13390e8e428492e6b676123914d9a68f7
Pasi Österman <pasi.osterman@tutanota.com>
2022-01-28 23:45:54 +0200

    tweaks to föri movement

--- Assets/core/scripts/movers/ForiMovement.cs
+++ Assets/core/scripts/movers/ForiMovement.cs
@@ -9,6 +9,7 @@ public class ForiMovement : Mover
     public Transform pointB;
 
     public float maxSpeed = 5.0f;
+    public float breakFactor = 0.3f;
 
     private Rigidbody rbody;
     private float movement;
@@ -18,20 +19,33 @@ public class ForiMovement : Mover
         rbody = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate() {
-        
-        if(movement > 0.0f){
+    private void FixedUpdate()
+    {
+        if (movement > 0.001f)
+        {
             MoveTowardsTargetTransform(pointA);
         }
-        else if(movement < 0.0f){
+        else if (movement < -0.001f)
+        {
             MoveTowardsTargetTransform(pointB);
+        }
+        else
+        {
+            Stop();
         }
     }
 
-    private void MoveTowardsTargetTransform(Transform target){
+    private void MoveTowardsTargetTransform(Transform target)
+    {
 
-        Vector3 dir =  (target.position - transform.position).normalized;
+        Vector3 dir = (target.position - transform.position).normalized;
         rbody.velocity = dir * maxSpeed;
+    }
+
+    private void Stop()
+    {
+        // rbody.velocity = rbody.velocity * 0.9f;
+        rbody.AddForce(-breakFactor * rbody.velocity);
     }
 
     public override void Move(Vector2 applyMovement)
