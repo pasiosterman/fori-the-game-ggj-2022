d174db8c334937756ed9de2e61c53d64ca880ef6
Pasi Österman <pasi.osterman@tutanota.com>
2022-01-28 18:45:28 +0200

    initial commit

---
+++ Assets/core/scripts/SomeScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SomeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
