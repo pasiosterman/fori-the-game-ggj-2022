2ad168762161fd91d7c0cea6bf478fea1267fb27
Pasi Österman <pasi.osterman@tutanota.com>
2022-01-30 02:00:34 +0200

    tweaks to how boats handle rotation on crashes

--- Assets/core/scripts/movers/ActorMover.cs
+++ Assets/core/scripts/movers/ActorMover.cs
@@ -21,16 +21,18 @@ public class ActorMover : Mover
         Vector3 desiredVel = new Vector3(MoveDir.x, 0.0f, MoveDir.z);
         desiredVel = desiredVel * maxSpeed;
         rbody.velocity = new Vector3(desiredVel.x, rbody.velocity.y, desiredVel.z);
+
+        rbody.angularVelocity = rbody.angularVelocity * 0.95f;
+
+        if (MoveDir.magnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(MoveDir, Vector3.up), Time.fixedDeltaTime * turnSpeed);
+        }
     }
 
     public override void Move(Vector3 applyMovement)
     {
         MoveDir = new Vector3(applyMovement.x, 0, applyMovement.z);
-
-        if (MoveDir.magnitude > 0.0f)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation,
-                Quaternion.LookRotation(MoveDir, Vector3.up), Time.deltaTime * turnSpeed);
-        }
     }
 }
--- Assets/core/scripts/movers/ForiMovement.cs
+++ Assets/core/scripts/movers/ForiMovement.cs
@@ -9,6 +9,7 @@ public class ForiMovement : Mover
     public Transform pointB;
     public float maxSpeed = 5.0f;
     public float maxVelocityChange = 1.0f;
+    public float turnSpeed = 2.0f;
 
     private float movement = 0.0f;
     private Rigidbody rbody;
@@ -34,6 +35,16 @@ public class ForiMovement : Mover
         }
     }
 
+    // private void Update() {
+
+    //     Vector3 dir = (pointA.position - transform.position).normalized;
+    //     if (dir.magnitude > 0.0f)
+    //     {
+    //         transform.rotation = Quaternion.Slerp(transform.rotation,
+    //             Quaternion.LookRotation(dir, Vector3.up), Time.deltaTime * turnSpeed);
+    //     }
+    // }
+
     private void MoveTowardsTargetTransform(Transform target)
     {
         if(rbody == null) return;
@@ -50,6 +61,19 @@ public class ForiMovement : Mover
         velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
         velocityChange.y = 0;
         rbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+
+        // To fix any collision rotation that may happen
+        Vector3 heading = (pointA.position - transform.position);
+        heading.y = 0.0f;
+        heading.Normalize();
+        if (heading.magnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(heading, Vector3.up), Time.fixedDeltaTime * turnSpeed);
+        }
+
+        rbody.angularVelocity = rbody.angularVelocity * 0.95f;
     }
 
     public override void Move(Vector3 applyMovement)
